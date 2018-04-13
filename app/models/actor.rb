de6ed11e97833b5fb0adc26ecb23598e16e6fc93
Ian Pollack <ianjpollack@gmail.com>
2018-04-13 03:25:14 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,15 +1,21 @@
 require 'pry'
 
 class Actor < ActiveRecord::Base
+<<<<<<< HEAD
   has_many :characters
   has_many :shows, through: :characters
   def change
     create_table :actors do |a|
+=======
+  def change
+    create_table :artists do |a|
+>>>>>>> 9328973cb288718d089af1afc3d37345d2b93d95
       a.string :first_name
       a.string :last_name
     end
   end
 
+<<<<<<< HEAD
   def full_name
     self.first_name + self.last_name
   end
@@ -17,4 +23,7 @@ class Actor < ActiveRecord::Base
   def list_roles
     self.characters
   end
+=======
+
+>>>>>>> 9328973cb288718d089af1afc3d37345d2b93d95
 end
